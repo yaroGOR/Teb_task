6cadc4e7d5b4751b0433bc71fa82a658b8140b83
Yarik <36842297+yaroGOR@users.noreply.github.com>
2022-08-15 13:26:38 +0300

    v4

--- tebteam/settings.py
+++ tebteam/settings.py
@@ -26,8 +26,7 @@ SECRET_KEY = 'django-insecure-xctgodq$5!37m&r612qi5nu=w*9r*hbcewba8q^$^8yf50bf^s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['herokuteb.herokuapp.com','127.0.0.1']
 
 # Application definition
 AUTH_USER_MODEL = 'accounts.Account'
